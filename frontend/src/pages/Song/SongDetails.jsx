43bacc226b9062814ccca9fa8752960db314300a
mmolina <mmolruib@g.educaand.es>
2024-12-17 20:40:57 +0100

    errores

--- frontend/src/pages/Song/SongDetails.jsx
+++ frontend/src/pages/Song/SongDetails.jsx
@@ -146,7 +146,7 @@ const SongDetails = () => {
               <div className="flex items-center gap-4">
                 <div className="relative">
                   <Avatar
-                    className="h-10 w-16 sm:h-20 sm:w-20 rounded-full object-cover border-2 border-gray-300 cursor-pointer"
+                    className="h-16 w-16 sm:h-20 sm:w-20 rounded-full object-cover border-2 border-gray-300 cursor-pointer"
                     src={
                       myReview.userId.profileImage ||
                       "/path/to/default-avatar.png"
