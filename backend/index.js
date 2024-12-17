f8b4e2ccd912271c221b4edba193ebeb597f4552
manumln <mmolruib@g.educaand.es>
2024-12-17 14:12:15 +0100

    index updated

--- backend/index.js
+++ backend/index.js
@@ -7,35 +7,53 @@ const cors = require("cors");
 const songRouter = require("./router/song");
 const userRouter = require("./router/users");
 
+// List of allowed origins
+const allowedOrigins = [
+  "https://eternal-music.vercel.app",
+  "http://localhost:5173/",
+];
+
+// CORS configuration
 app.use(
   cors({
-    origin: "https://eternal-music.vercel.app", // Cambia este origen al dominio de tu frontend en producción
-    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos HTTP permitidos
-    credentials: true, // Habilitar cookies y encabezados de autenticación
+    origin: function (origin, callback) {
+      // Allow requests with no origin (e.g., mobile apps or curl requests)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"], // HTTP methods allowed
+    credentials: true, // Enable cookies and authentication headers
   })
 );
 
-
 app.use(express.json());
 
+// Database connection
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Connected to Database"))
-  .catch(() => console.log("Error Connecting to Database"));
+  .catch((err) => console.log("Error Connecting to Database:", err));
 
+// Serve static files
 const path = require("path");
 app.use("/public", express.static(path.join(__dirname, "./public")));
 app.use(express.urlencoded({ extended: false }));
 
+// Routers
 app.use("/songs", songRouter);
 app.use("/users", userRouter);
 
+// Home route
 app.get("/", (req, res) => {
   res.json({
     message: "This is home route",
   });
 });
 
+// Error handling middleware
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong" } = err;
   res.status(statusCode).json({
@@ -43,6 +61,7 @@ app.use((err, req, res, next) => {
   });
 });
 
+// Start the server
 app.listen(port, () => {
-  console.log(`There server is running at ${port}`);
+  console.log(`The server is running at ${port}`);
 });
